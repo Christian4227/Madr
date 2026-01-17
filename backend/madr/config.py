bea1f79bfaf361ae19c417a0b1382b186977c0a6
luiz_loon <luiz_loon@yahoo.com.br>
2026-01-17 18:21:19 -0300

    refact: permitir lista de cors

--- backend/madr/config.py
+++ backend/madr/config.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-from typing import List
 
+from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-BASE_DIR = Path(__file__).resolve().parent.parent  # ajuste conforme estrutura
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 class Settings(BaseSettings):
@@ -18,18 +18,11 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
-    CORS_ORIGINS: List[str]
+    CORS_ORIGINS: list[str]
 
-
-# class Settings(BaseSettings):
-#     model_config = SettingsConfigDict(
-#         env_file='.env', env_file_encoding='utf-8'
-#     )
-#     POSTGRES_USER: str
-#     POSTGRES_PASSWORD: str
-#     POSTGRES_DB: str
-#     DATABASE_URL: str
-#     SECRET_KEY: str
-#     ALGORITHM: str
-#     ACCESS_TOKEN_EXPIRE_MINUTES: int
-#     CORS_ORIGINS: List[str]
+    @field_validator('CORS_ORIGINS', mode='before')
+    @classmethod
+    def parse_cors(cls, v):
+        if isinstance(v, str):
+            return [x.strip() for x in v.split(',')]
+        return v
